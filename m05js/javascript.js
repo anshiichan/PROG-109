530584328a7e66d592c2eb6a8d2cd38a0da5e084
anshiichan <77084444+anshiichan@users.noreply.github.com>
2021-02-06 21:31:57 -0800

    Update javascript.js

--- m05js/javascript.js
+++ m05js/javascript.js
@@ -1,28 +1,36 @@
 var value = prompt("Please enter a number 0-10", "example: 1");  //Prompt to obtain user submitted value
 
-if (value == null) {
-  prompt("Please enter a number 0-10", "example: 1");
+/* if (value === null) {
+  var value = prompt("Please enter a number 0-10", "example: 1");
 }
 
-var msg = '<h2>Multiplication Table</h2>';
+elseif (value > 10) {
+  var value = prompt("Please enter a number 0-10", "example: 1");
+}
 
-var table = 3;             // Unit of table
-var operator = 'addition'; // Type of calculation
+else (value < 0) {
+  var value = prompt("Please enter a number 0-10", "example: 1");
+} */
+
+var table = value;             // Unit of table, from user inputted number
+var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var msg = '<h2>Multiplication Table</h2>';              // Message
 
-if (operator === 'addition') {
+/* if (operator === 'addition') {
   // Do addition
   while (i < 11) {
     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
     i++;
   }
-} else {
+} */
+if (operator === 'multiplication') {
   // Do multiplication
-  while (i < 11) {
+  while (i <= value) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
+
 }
 
 // Write the message into the page
